d8e15d4e7acca82f77f62ab7d361a0c6e559959b
jnow-87 <jan.nowotsch@gmail.com>
2023-10-22 00:00:50 +0200

    [timer: add destroy and ack]
    Add timer_destroy() and timer_ack() to have a more consistent interface.
    Update keys to use the new functions.

--- utils/timer.c
+++ utils/timer.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include <unistd.h>
 #include <sys/timerfd.h>
 #include <utils/timer.h>
 
@@ -17,14 +19,26 @@ int timer_init(void){
 	return fd;
 }
 
+void timer_destroy(int fd){
+	if(fd != -1)
+		close(fd);
+}
+
 int timer_set(int fd, long int ms){
 	struct itimerspec time;
 
 
-	time.it_value.tv_nsec = ms * 1000000;
+	time.it_value.tv_nsec = (ms % 1000) * 1000000;
+	time.it_value.tv_sec = ms / 1000;
 	time.it_interval.tv_nsec = time.it_value.tv_nsec;
-	time.it_value.tv_sec = 0;
-	time.it_interval.tv_sec = 0;
+	time.it_interval.tv_sec = time.it_value.tv_sec;
 
 	return -(timerfd_settime(fd, 0, &time, 0x0) != 0);
+}
+
+void timer_ack(int fd){
+	uint64_t data;
+
+
+	read(fd, &data, sizeof(data));
 }
